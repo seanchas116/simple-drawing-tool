6381d4810b32a5ba9b5cfc76fbdbbc6b88823177
Ryohei Ikegami <iofg2100@gmail.com>
2023-11-07 20:14:13 +0900

    Refactor

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,38 +7,31 @@ import { SignIn } from "./SignIn";
 
 const drawing = new Drawing("test");
 
+const tools = [
+  { type: "select", icon: "icon-park-outline:move-one" },
+  { type: "rect", icon: "icon-park-outline:square" },
+  { type: "ellipse", icon: "icon-park-outline:round" },
+  { type: "arrow", icon: "icon-park-outline:arrow-right-up" },
+] as const;
+
 export const App = observer(() => {
   return (
     <div className="fixed inset-0 w-screen h-screen">
       <Canvas drawing={drawing} />
       <div className="absolute left-[50%] bottom-4 translate-x-[-50%] shadow-xl border border-gray-200 rounded-full px-3 py-1 flex">
-        <button
-          className="p-2 rounded-full aria-selected:bg-blue-500 aria-selected:text-white"
-          aria-selected={drawing.tool === "rect"}
-          onClick={() => {
-            drawing.tool = drawing.tool === "rect" ? "select" : "rect";
-          }}
-        >
-          <Icon icon="icon-park-outline:square" />
-        </button>
-        <button
-          className="p-2 rounded-full aria-selected:bg-blue-500 aria-selected:text-white"
-          aria-selected={drawing.tool === "ellipse"}
-          onClick={() => {
-            drawing.tool = drawing.tool === "ellipse" ? "select" : "ellipse";
-          }}
-        >
-          <Icon icon="icon-park-outline:round" />
-        </button>
-        <button
-          className="p-2 rounded-full aria-selected:bg-blue-500 aria-selected:text-white"
-          aria-selected={drawing.tool === "arrow"}
-          onClick={() => {
-            drawing.tool = drawing.tool === "arrow" ? "select" : "arrow";
-          }}
-        >
-          <Icon icon="icon-park-outline:arrow-right-up" />
-        </button>
+        {tools.map((tool) => {
+          return (
+            <button
+              className="p-2 rounded-full aria-selected:bg-blue-500 aria-selected:text-white"
+              aria-selected={drawing.tool === tool.type}
+              onClick={() => {
+                drawing.tool = tool.type;
+              }}
+            >
+              <Icon icon={tool.icon} />
+            </button>
+          );
+        })}
       </div>
       <div className="absolute right-4 bottom-4 shadow-xl border border-gray-200 rounded-full px-3 py-1 flex">
         <button className="p-2">
