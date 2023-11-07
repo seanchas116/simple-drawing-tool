cd3cada587a1d657556b3e505e348fcfef97cf7b
Ryohei Ikegami <iofg2100@gmail.com>
2023-11-07 16:59:44 +0900

    Add toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="fixed inset-0 w-screen h-screen">
-      <div className="absolute left-[50%] bottom-2 translate-x-[-50%] shadow-xl border border-gray-200 rounded-full p-2">
+      <div className="absolute left-[50%] bottom-4 translate-x-[-50%] shadow-xl border border-gray-200 rounded-full p-2">
         TODO: Tools
       </div>
     </div>
