8a7670380bd868970ed9a8e8b5ee92f0c63a92d1
Ryohei Ikegami <iofg2100@gmail.com>
2023-11-07 17:42:14 +0900

    Add tool buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Icon } from "@iconify/react";
+
 export default function App() {
   return (
     <div className="fixed inset-0 w-screen h-screen">
-      <div className="absolute left-[50%] bottom-4 translate-x-[-50%] shadow-xl border border-gray-200 rounded-full p-2">
-        TODO: Tools
+      <div className="absolute left-[50%] bottom-4 translate-x-[-50%] shadow-xl border border-gray-200 rounded-full px-3 py-1 flex">
+        <button className="p-2">
+          <Icon icon="icon-park-outline:square" />
+        </button>
+        <button className="p-2">
+          <Icon icon="icon-park-outline:round" />
+        </button>
+        <button className="p-2">
+          <Icon icon="icon-park-outline:arrow-right-up" />
+        </button>
       </div>
     </div>
   );
