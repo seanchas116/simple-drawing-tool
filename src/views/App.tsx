2fbb5a92c2041ccb1981188293199af29ec8d6ac
Ryohei Ikegami <iofg2100@gmail.com>
2023-11-09 11:52:37 +0900

    select-none

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,7 +5,7 @@ import { DrawingApp } from "./DrawingApp";
 
 export const App = observer(() => {
   return (
-    <div className="fixed inset-0 w-screen h-screen">
+    <div className="fixed inset-0 w-screen h-screen select-none">
       {auth.state.type === "authenticated" && <DrawingApp />}
       {auth.state.type === "unauthenticated" && <SignIn />}
     </div>
